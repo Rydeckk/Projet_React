8ad5f58012e807e5ec1f014d936a1f55e13b2e48
yahya <yahyamamoudou.sow.etu@univ-lille.fr>
2024-06-12 11:35:22 +0200

    Maison-horrique

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
-import "../class/classes.css"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import { GlobalStyles } from "./styles/GlobalStyle";
+import ReservationForm from "./pages/ReservationForm";
+import SessionDetail from "./pages/SessionDetail";
+import ContactForm from "./pages/ContactForm";
+import Navbar from "./components/Navbar";
 
-export function App() {
-  const [isMenuTopVisible, setIsMenuTopVisible] = useState(true)
+function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <GlobalStyles/>
+      <Navbar/>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>}/>
+        <Route path = "/reservation" element = {<ReservationForm/>}/>
+        <Route path = "/session/:id" element= {<SessionDetail/>}/>
+        <Route path = "/contact" element= {<ContactForm/>}/>
+      </Routes>
+    </Router>
+
   );
-}
+};
+
+export default App;
