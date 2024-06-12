49246519b164f904bdd94bf931bdf940636ee4ec
Rydeckk <romainguiffantwork@hotmail.com>
2024-06-12 11:31:05 +0200

    newAPP

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "../class/classes.css"
 
 export function App() {
-  const [isMenuTopVisible, setIsMenuTopVisible] = useState(true)
   return (
     <div>
       
