cc2104c43601b03a2ce51bff2f59f71eecaa4925
Rydeckk <romainguiffantwork@hotmail.com>
2024-06-14 10:29:25 +0200

    UpdateFooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { FooterContainer } from "../styles/Footer.styled";
+import { DivLogo, DivSpan, DivText, DivTextContext, FooterContainer, LinkText } from "../styles/Footer.styled";
 
 const Footer = () => {
     return (
     <FooterContainer>
         <div style={{display: "flex",justifyContent:"space-between"}}>
-            <div style={{display: "flex",width: "192px"}}></div>
-            <div style={{display: "flex", justifyContent:"center",flexGrow:1}}>
-                <div style={{display: "flex", padding:"15px"}}>
-                    <a href="/session/:id" style={{textDecoration: "none", color: "#FFF"}}>Session</a>
-                </div>
-                <div style={{display: "flex", padding:"15px"}}>
-                    <a href="/legal/" style={{textDecoration: "none", color: "#FFF"}}>Mentions légales</a>
-                </div>
-                <div style={{display: "flex", padding:"15px",textDecoration: "none"}}>
-                    <a href="/contact/" style={{textDecoration: "none", color: "#FFF"}}>Contact</a>
-                </div>
-            </div>
-            <div style={{display: "flex", justifyContent:"flex-end", marginRight: "20px"}}>
+            <DivSpan></DivSpan>
+            <DivTextContext>
+                <DivText>
+                    <LinkText href="/session/:id">Session</LinkText>
+                </DivText>
+                <DivText>
+                    <LinkText href="/legal/">Mentions légales</LinkText>
+                </DivText>
+                <DivText>
+                    <LinkText href="/contact/">Contact</LinkText>
+                </DivText>
+            </DivTextContext>
+            <DivLogo>
                 <div>
                     <img src="icon-facebook.png"></img>
                 </div>
@@ -30,7 +30,7 @@ const Footer = () => {
                 <div>
                     <img src="icon-tiktok.png"></img>
                 </div>
-            </div>
+            </DivLogo>
         </div>
         <p>&copy; 2024 La Maison Horrifique. Tous droits réservés.</p>
     </FooterContainer>
--- src/styles/Footer.styled.ts
+++ src/styles/Footer.styled.ts
@@ -9,3 +9,36 @@ export const FooterContainer = styled.footer
  bottom: 0;
  width: 100%;
 `
+
+export const DivText = styled.div
+`
+display: flex; 
+padding: 15px;
+`
+
+export const DivLogo = styled.div
+`
+display: flex; 
+justify-Content: flex-end;
+margin-Right: 20px;
+width: 200px;
+`
+
+export const DivSpan = styled.div
+`
+display: flex;
+width: 200px;
+`
+
+export const DivTextContext = styled.div
+`
+display: flex; 
+justify-Content: center;
+flex-Grow:1;
+`
+
+export const LinkText = styled.a
+`
+color: #FFF;
+text-decoration: none;
+`
